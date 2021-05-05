b3cb8868d0d86a20d6b048b8ac46633f89f0b117
Luis <oneillvilla@gmail.com>
2021-05-04 20:33:55 -0400

    Adding changes for first step

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
