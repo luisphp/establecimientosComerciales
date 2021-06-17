96f847c48b8be4800d8581c3b763a500160806f3
Luis <oneillvilla@gmail.com>
2021-06-17 04:21:06 -0400

    boton para acceder a creacion y edicion

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const EST_CREATE = '/establecimiento/create';
+    public const INICIO = '/';
 
     /**
      * The controller namespace for the application.
